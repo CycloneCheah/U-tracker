4903adcf587926f87f2fdd514dcc86adf52ab696
CycloneCheah <95611062+CycloneCheah@users.noreply.github.com>
2021-12-08 22:51:54 +0800

    Newest

--- view.js
+++ view.js
@@ -6,20 +6,39 @@ Description: Contains global codes that will run when view.js is loaded
 
 // For data in the table
 
-// Access the cells by Id
-let tableTitle = document.getElementById("tableTitle");
-let tableDueDate = document.getElementById("tableDueDate");
-let tableWeightage = document.getElementById("tableWeightage");
-let tablePercentageCompleted = document.getElementById("tablePercentageCompleted");
-
 
 // Retrieve stored indexes from local storage 
 let assignmentIndex = localStorage.getItem(ASSIGNMENT_INDEX_KEY);
-let assignment = list.getAssignment(assignmentIndex);
-assignment = new Assignment("PornHub", 8, 12, 2021, 50);
+let assignment = new Assignment();
+assignment = list.getAssignment(assignmentIndex);;
 
 
-tableTitle.innerHTML = assignment.title;
-tableDueDate.innerHTML = assignment.dueDate;
-tableWeightage.innerHTML = assignment.weightage;
-tablePercentageCompleted.innerHTML = assignment.completedPercentage + "%";
+    let assignmentDetailsTableRef = document.getElementById('assignmentDetailsTable');
+    // print table to html
+    // initial table code
+    let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
+    <thead>
+      <tr>
+        <th class="mdl-data-table__cell--non-numeric">Asssessment</th>
+        <th class="mdl-data-table__cell--non-numeric">Due Date</th>
+        <th class="mdl-data-table__cell--non-numeric">Weightage</th>
+        <th class="mdl-data-table__cell--non-numeric">Completion Percentage (%)</th>
+      </tr>
+    </thead>
+    <tbody>
+    `;
+  // printing each assignment 
+    
+  // test if it is an upcoming or past trip
+          output += `<tr>
+                <td class="mdl-data-table__cell--non-numeric">${assignment.title}</td>
+                <td class="mdl-data-table__cell--non-numeric">${assignment.dueDate}</td>
+                <td class="mdl-data-table__cell--non-numeric">${assignment.weightage}</td>
+                <td class="mdl-data-table__cell--non-numeric">${assignment.completedPercentage}</td>
+              </tr> `
+      // close the table
+      output += `</tbody>
+      </table>`
+      // return results
+      assignmentDetailsTableRef.innerHTML = output;
+
