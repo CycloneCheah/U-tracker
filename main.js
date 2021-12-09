d7d43c0aa028e5694112f599e41893316d1ce981
bonzonic <76901263+bonzonic@users.noreply.github.com>
2021-12-09 17:03:05 +0800

    die me

--- main.js
+++ main.js
@@ -170,19 +170,109 @@ function view(index) {
     <tbody>
     `;
   // printing each assignment 
+  // Only dates are allowed for this, month is not considered yet
+    var arr = [];
     for (let i = 0; i < data.length; i++) {
-  // test if it is an upcoming or past trip
-          output += `<tr>
-                <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br> </td>
-              </tr> `
-        }
-      // close the table
-      output += `</tbody>
-      </table>`
-      // return results
-      listTableRef3.innerHTML = output;
+      arr.push(new Date(data[i].dueDate.substring(0, 10)).getDate());
     }
-  // code to run function
+    var anotherarr = [];
+    for (let i = 0; i < data.length; i++) {
+      anotherarr.push(data[i].title);
+    }
+    function insertionSort(arr, anotherarr){
+      for(let i = 1; i < arr.length; i++){
+          var mark = arr[i];
+          var mark1 = anotherarr[i];
+          var j = i - 1;
+          while (j >= 0 && arr[j] > mark){
+              console.log("In", i, j);
+              arr[j + 1] = arr[j];
+              anotherarr[j+1] = anotherarr[j];
+              j--;
+          }
+          anotherarr[j+1] = mark1;
+          arr[j+1] = mark;
+      }
+      return arr;
+    }
+    arr = insertionSort(arr, anotherarr);
+    let currentDate = new Date();
+    currentDate = currentDate.getDate();
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = parseInt(arr[i]) - parseInt(currentDate) + 1;
+    }
+    // average number of days @Step 4
+    var average = Math.floor(Math.max(...arr)/arr.length);
+    for (let i=0; i < arr.length; i++) {
+      if (arr[i] < average) {
+        var residue = average - arr[i];
+        arr[i+1] += residue;
+      }
+      else {
+        if (average != 0) {
+        arr[i] = average;
+        }
+      }
+    }
+          // Step 5
+          // if update, the algorithm will die
+    // document.getElementById("arrr").innerHTML = arr[0]; - 0
+    var days = 0;
+    for (let i = 0; i < arr.length; i++) {
+      var progress = 0;
+      for (let j = 0; j < arr[i]-days; j++) { 
+        output += `<tr>
+        <td class="mdl-data-table__cell--non-numeric">${gettingDate(days)}<br>${anotherarr[i]}<br>Percentage to be done:${gettingPercentage(arr[i]-days, progress)}</td>
+      </tr> `
+      progress += 100/arr[i]-days;
+        }
+        if ((i+1<arr.length) && arr[i+1]==days+1) {
+          //pass;
+        }
+        else {
+          days += 1;
+      }
+      }
+        // close the table
+          output += `</tbody>
+          </table>`
+          
+    // var testing = 0;
+    // for (let i = 0; i < data.length; i++) {
+    //   // test if it is an upcoming or past trip
+    //           output += `<tr>
+    //                 <td class="mdl-data-table__cell--non-numeric">${gettingDate(i)}<br>${data[i].title}<br>Percentage to be done:${gettingPercentage(i)}</td>
+    //               </tr> `
+    //               testing += 1;
+    //         }
+    //       // close the table
+    //       output += `</tbody>
+    //       </table>`
+    // var testing = 0;
+    // for (let i = 0; i < data.length; i++) {
+    //   for (let j = 0; j < arr[i]; j++) {
+    //   // test if it is an upcoming or past trip
+    //           output += `<tr>
+    //                 <td class="mdl-data-table__cell--non-numeric">${gettingDate(i)}<br>${data[i].title}<br>Percentage to be done:${gettingPercentage(i)}</td>
+    //               </tr> `
+    //               testing += 1;
+    //         }
+    //       }
+    //       // close the table
+    //       output += `</tbody>
+    //       </table>`
+
+    function gettingPercentage(number, progress) {
+      return progress + "%" + "~>" + (progress + 100/number) + "%";
+    }
+    function gettingDate(number) {
+      var date = new Date();
+      var res = parseInt(date.getDate())+number + "-" + parseInt(date.getMonth()+1) + "-" + parseInt(date.getFullYear());
+      return res; 
+    }
+   // return results
+    listTableRef3.innerHTML = output;
+  }
   displayList3(list._queue);
 
   function displayList4(data) {
@@ -214,12 +304,4 @@ function view(index) {
   // code to run function
   displayList4(list._queue);
 
-  function recommendedlist() {
-    let rList = document.getElementById("recommended");
-
-    // access current date and deadline of assignment, find how long is it and put in an array according to position of the assignments
-    // compare to weightage and split accordingly
-    // Algorithm of splitting:
-    // 
-
-  }
+ 
