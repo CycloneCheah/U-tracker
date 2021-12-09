a80a8bed30cfad0be8165da89eae16c84ad78d90
CycloneCheah <95611062+CycloneCheah@users.noreply.github.com>
2021-12-09 14:23:50 +0800

    updates

--- main.js
+++ main.js
@@ -1,103 +1,30 @@
 "use strict";
 function view(index) {
-    // Storing data of index using key ASSIGNMENT_INDEX_KEY in local storage
-    localStorage.setItem(ASSIGNMENT_INDEX_KEY,index);
-    // Direct user to view.html page
-    window.location.assign("view.html");
+  // Storing data of index using key ASSIGNMENT_INDEX_KEY in local storage
+  localStorage.setItem(ASSIGNMENT_INDEX_KEY, index);
+  // Direct user to view.html page
+  window.location.assign("view.html");
+}
+
+function markDone(index) {
+  let r = confirm("Mark as done?"); // r ← confirm method() to confirm 'mark' action with user 
+  if (r == true) {
+    list.removeAssignment(index); // Using removeAssignment method to remove assignment from completed list
+    updateLocalStorage(APP_DATA_KEY, list); // Update local Storage with the latest data
+    displayList2(list._queue); // Call Task 10 function
   }
-
- function markDone(index) {
-    let r = confirm("Mark as done?"); // r ← confirm method() to confirm 'mark' action with user 
-    if (r == true) {
-     list.removeAssignment(index); // Using removeAssignment method to remove assignment from completed list
-      updateLocalStorage(APP_DATA_KEY, list); // Update local Storage with the latest data
-      displayList2(list._queue); // Call Task 10 function
-    }
-    else {
-      return 0;
-    }
+  else {
+    return 0;
   }
-// function displayCurrentList(data) {
-//     let listContentRef = document.getElementById("listContent");
-//     listContentRef.innerHTML = "";
-//     let output = "";
-  
-//     for (let i = 0; i < data.length; i++) {
-//       output += `<ul class=\"mdl-list\">\n<h4>Queue ${i + 1} </h4>\n`;
-//         let title = data[i]._title;
-//         // Insert dynamic values for the onclick attributes for view 
-//         output += `<li class="mdl-list__item mdl-list__item--three-line">\n
-//         <span class="mdl-list__item-primary-content">\n
-//             <i class="material-icons mdl-list__item-avatar">assignment</i>\n
-//             <span>${title}</span>\n
-//         </span>\n
-//         <span class="mdl-list__item-secondary-content">\n
-//             <a class="mdl-list__item-secondary-action" onclick="view(${i})"><i\n
-//                     class="material-icons">info</i></a>\n
-//         </span>\n
-//       </li>\n`;
-//       output += "</ul>";
-//     }
-//     listContentRef.innerHTML = output;
-//   }// /
-  
-//   displayCurrentList(list._queue); 
+}
 
 
-//   function displayCompletedList(data) {
+function displayList(data) {
 
-//     // Check if user is logged in
-//     let completedListTableRef = document.getElementById('completedListTable');
-//     // print table to html
-//       let trips = thisUser.tripList.trips;
-//        // date variables
-//       let currentDate = new Date();
-//       let currentYear = currentDate.getFullYear();
-//       let currentMonth = currentDate.getMonth() + 1;
-//       let currentDay = currentDate.getDate();
-//       let currentDateString = `${currentYear}-${currentMonth}-${currentDay}`
-//   // initial table code
-//       let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
-//     <thead>
-//       <tr>
-//         <th class="mdl-data-table__cell--non-numeric">Completed List</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//     `;
-//   // printing each trip
-//     for (let i = 0; i < data.length; i++) {
-//         let assignmentDate = data[i].date;
-//   // test if it is an upcoming or past trip
-//         if (assignmentDate < currentDateString) {
-  
-//           console.log(currentDate);
-  
-//           output += `<tr>
-//                 <td class="mdl-data-table__cell--non-numeric">${trips[i].country}</td>
-//                 <td>${data[i].title}</td>
-//                 <td><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick = "viewTrip(${i})">View on map</button></td>
-//                 <td><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick = "removeTrip(${i})">Delete trip</button></td>
-//               </tr>
-//               `
-//         }
-//       }
-//       // close the table
-//       output += `</tbody>
-//       </table>`
-//       // return results
-//       completedListTableRef.innerHTML = output;
-//     }
-  
-//   // code to run function
-//   displayCompletedList(list._queue);
-
-  function displayList(data) {
-
-    let listTableRef = document.getElementById('listTable');
-    // print table to html
-    // initial table code
-    let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
+  let listTableRef = document.getElementById('listTable');
+  // print table to html
+  // initial table code
+  let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
     <thead>
       <tr>
         <th class="mdl-data-table__cell--non-numeric">Assessment List</th>
@@ -107,29 +34,29 @@ function view(index) {
     <tbody>
     `;
   // printing each assignment 
-    for (let i = 0; i < data.length; i++) {
-          if (data[i].completedPercentage >= 0 && data[i].completedPercentage < 100){
-          output += `<tr>
-                <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br>Due Date: ${data[i].dueDate.substring(0,10)}</td>
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].completedPercentage >= 0 && data[i].completedPercentage < 100) {
+      output += `<tr>
+                <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br>Due Date: ${data[i].dueDate.substring(0, 10)}</td>
                 <td><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick = "view(${i})">View</button></td>
               </tr> `
-          }
     }
-      // close the table
-      output += `</tbody>
+  }
+  // close the table
+  output += `</tbody>
       </table>`
-      // return results
-      listTableRef.innerHTML = output;
-    }
-  // code to run function
-  displayList(list._queue);
+  // return results
+  listTableRef.innerHTML = output;
+}
+// code to run function
+displayList(list._queue);
 
-    function displayList2(data) {
+function displayList2(data) {
 
-    let listTableRef2 = document.getElementById('listTable2');
-    // print table to html
-    // initial table code
-    let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
+  let listTableRef2 = document.getElementById('listTable2');
+  // print table to html
+  // initial table code
+  let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
     <thead>
       <tr>
         <th class="mdl-data-table__cell--non-numeric">Completed List</th>
@@ -139,29 +66,29 @@ function view(index) {
     <tbody>
     `;
   // printing each assignment 
-    for (let i = 0; i < data.length; i++) {
-          if (data[i].completedPercentage == 100){
-          output += `<tr>
-          <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br>Due Date: ${data[i].dueDate.substring(0,10)}</td>
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].completedPercentage == 100) {
+      output += `<tr>
+          <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br>Due Date: ${data[i].dueDate.substring(0, 10)}</td>
           <td><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick = "markDone(${i})">DONE</button></td>
               </tr> `
-          }
-        }
-      // close the table
-      output += `</tbody>
-      </table>`
-      // return results
-      listTableRef2.innerHTML = output;
     }
-  // code to run function
-  displayList2(list._queue);
+  }
+  // close the table
+  output += `</tbody>
+      </table>`
+  // return results
+  listTableRef2.innerHTML = output;
+}
+// code to run function
+displayList2(list._queue);
 
-  function displayList3(data) {
+function displayList3(data) {
 
-    let listTableRef3 = document.getElementById('listTable3');
-    // print table to html
-    // initial table code
-    let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
+  let listTableRef3 = document.getElementById('listTable3');
+  // print table to html
+  // initial table code
+  let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
     <thead>
       <tr>
         <th class="mdl-data-table__cell--non-numeric">Recommendation List</th>
@@ -170,27 +97,27 @@ function view(index) {
     <tbody>
     `;
   // printing each assignment 
-    for (let i = 0; i < data.length; i++) {
-  // test if it is an upcoming or past trip
-          output += `<tr>
+  for (let i = 0; i < data.length; i++) {
+    // test if it is an upcoming or past trip
+    output += `<tr>
                 <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br> </td>
               </tr> `
-        }
-      // close the table
-      output += `</tbody>
+  }
+  // close the table
+  output += `</tbody>
       </table>`
-      // return results
-      listTableRef3.innerHTML = output;
-    }
-  // code to run function
-  displayList3(list._queue);
+  // return results
+  listTableRef3.innerHTML = output;
+}
+// code to run function
+displayList3(list._queue);
 
-  function displayList4(data) {
+function displayList4(data) {
 
-    let listTableRef4 = document.getElementById('listTable4');
-    // print table to html
-    // initial table code
-    let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
+  let listTableRef4 = document.getElementById('listTable4');
+  // print table to html
+  // initial table code
+  let output = `<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
     <thead>
       <tr>
         <th class="mdl-data-table__cell--non-numeric">Recent Updates</th>
@@ -199,27 +126,27 @@ function view(index) {
     <tbody>
     `;
   // printing each assignment 
-    for (let i = 0; i < data.length; i++) {
-  // test if it is an upcoming or past trip
-          output += `<tr>
+  for (let i = 0; i < data.length; i++) {
+    // test if it is an upcoming or past trip
+    output += `<tr>
                 <td class="mdl-data-table__cell--non-numeric">${data[i].title}<br> </td>
               </tr> `
-        }
-      // close the table
-      output += `</tbody>
-      </table>`
-      // return results
-      listTableRef4.innerHTML = output;
-    }
-  // code to run function
-  displayList4(list._queue);
-
-  function recommendedlist() {
-    let rList = document.getElementById("recommended");
-
-    // access current date and deadline of assignment, find how long is it and put in an array according to position of the assignments
-    // compare to weightage and split accordingly
-    // Algorithm of splitting:
-    // 
-
   }
+  // close the table
+  output += `</tbody>
+      </table>`
+  // return results
+  listTableRef4.innerHTML = output;
+}
+// code to run function
+displayList4(list._queue);
+
+function recommendedlist() {
+  let rList = document.getElementById("recommended");
+
+  // access current date and deadline of assignment, find how long is it and put in an array according to position of the assignments
+  // compare to weightage and split accordingly
+  // Algorithm of splitting:
+  // 
+
+}
